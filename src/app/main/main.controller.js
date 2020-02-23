512ff3fae5adf91fc906faac80f6db81d193a516
sheikmubarakj <sheikmubarakj@gmail.com>
2020-02-23 15:46:06 +0530

    files modified

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -14,10 +14,62 @@
     vm.creationDate = 1582442504913;
     vm.showToastr = showToastr;
 
+    vm.init = () => {
+      vm.isViewSpeech = true;
+      vm.speeches = [];
+      let _localStorageItem = localStorage.getItem('speechList');
+      if (_localStorageItem) {
+        try {
+          vm.speeches = JSON.parse(_localStorageItem);
+        } catch (e) {
+          console.log('Invalid localstorage item', e);
+          localStorage.removeItem('speechList');
+        }
+      }  
+    };
+
+    vm.init();
+
     activate();
+
+    vm.saveLocalDB = () => {
+      localStorage.setItem('speechList', JSON.stringify(vm.speeches));
+      // vm.init();
+    };
+
+    vm.selectSpeech = (speech) => {
+      vm.selectedSpeech = speech;
+    };
+
+    vm.removeSpeech = (speech) => {
+      if (!speech || !speech.id) return;
+      vm.speeches.splice(vm.speeches.indexOf(speech), 1);
+      vm.saveLocalDB();
+      vm.reset();
+    };
+
+    vm.addSpeech = (newSpeech) => {
+      if (newSpeech.id) {
+        
+      } else {
+        newSpeech.id = vm.speeches.length + 1;
+        vm.speeches.push(newSpeech);
+      }
+
+      
+      vm.saveLocalDB();
+      vm.reset();
+    };
+
+
+
+    vm.reset = () => {
+      vm.selectedSpeech = {}
+    };
 
     function activate() {
       getWebDevTec();
+      
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
@@ -30,10 +82,23 @@
 
     function getWebDevTec() {
       vm.awesomeThings = webDevTec.getTec();
-
       angular.forEach(vm.awesomeThings, function(awesomeThing) {
         awesomeThing.rank = Math.random();
       });
     }
   }
 })();
+/*
+1. form $dirty shoud be reset after save and delete
+2. edited value should update in list
+//Functionality
+1. Button name - Create/Update
+2. dont allow to create new one in my speech list
+3. overall search
+
+
+//Design
+1. form allignment
+2. active selected item
+
+*/
