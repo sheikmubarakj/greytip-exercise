091e883645a606f982bf6d70e6b93253893f2816
sheikmubarakj <sheikmubarakj@gmail.com>
2020-02-23 12:56:32 +0530

    inital commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('greyTip')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
