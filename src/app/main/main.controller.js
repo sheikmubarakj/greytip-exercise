64bf351b0f35f53c94901e18d8a16ff7325b6ae1
sheikmubarakj <sheikmubarakj@gmail.com>
2020-02-23 22:58:13 +0530

    removed es6

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -14,14 +14,16 @@
     vm.creationDate = 1582442504913;
     vm.showToastr = showToastr;
 
-    vm.getClonedObject = (speech) => {
+    // vm.getClonedObject = (speech) => {
+    vm.getClonedObject = function (speech) {
       return Object.assign({}, speech);
     };
 
-    vm.init = () => {
+    // vm.init = () => {
+    vm.initialize = function () {
       vm.isViewSpeech = true;
       vm.speeches = [];
-      let _localStorageItem = localStorage.getItem('speechList');
+      var _localStorageItem = localStorage.getItem('speechList');
       if (_localStorageItem) {
         try {
           vm.speeches = JSON.parse(_localStorageItem);
@@ -33,33 +35,38 @@
       }  
     };
 
-    vm.newSpeech = () => {
+    // vm.newSpeech = () => {
+    vm.newSpeech = function () {
       vm.reset();
       vm.isViewSpeech = false;
       vm.selectedSpeech = {};
     };
 
-    vm.mySpeech = () => {
+    // vm.mySpeech = () => {
+    vm.mySpeech = function () {
       vm.reset();
       vm.isViewSpeech = true;
       vm.selectedSpeech = vm.getClonedObject(vm.speeches[0]);
     };
 
-    vm.init();
+    vm.initialize();
 
     activate();
 
-    vm.saveLocalDB = () => {
+    // vm.saveLocalDB = () => {
+    vm.saveLocalDB = function () {
       localStorage.setItem('speechList', JSON.stringify(vm.speeches));
     };
 
-    vm.selectSpeech = (speech) => {
+    // vm.selectSpeech = (speech) => {
+    vm.selectSpeech = function (speech) {
       vm.selectedSpeech = vm.getClonedObject(speech);
     };
 
-    vm.removeSpeech = (speech) => {
+    // vm.removeSpeech = (speech) => {
+    vm.removeSpeech = function (speech) {
       if (!speech || !speech.id) return;
-      let _index = vm.speeches.findIndex((item) => {
+      var _index = vm.speeches.findIndex( function (item) {
         return item.id === speech.id;
       })
       vm.speeches.splice(_index, 1);
@@ -67,9 +74,10 @@
       vm.reset();
     };
 
-    vm.addSpeech = (newSpeech) => {
+    // vm.addSpeech = (newSpeech) => {
+    vm.addSpeech = function (newSpeech) {
       if (newSpeech.id) {
-        let _speech = vm.speeches.find((item) => {
+        var _speech = vm.speeches.find(function (item) {
           return item.id === newSpeech.id;
         });
         _speech.description = newSpeech.description
@@ -86,7 +94,8 @@
 
 
 
-    vm.reset = () => {
+    // vm.reset = () => {
+    vm.reset = function () {
       vm.selectedSpeech = {}
     };
 
@@ -112,7 +121,7 @@
   }
 })();
 /*
-1. form $dirty shoud be reset after save and delete
+1. form $dirty shoud be reset after save and devare
 2. edited value should update in list
 //Functionality
 1. Button name - Create/Update
